5cd023c50d5c5895bfc1ac7991770b68fd59a130
Ngo Duc Toan <ngoductoan12102018@gmail.com>
2026-03-04 02:55:12 +0700

    Update tab component UI

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const tabs = document.querySelectorAll(".tab");
 const content = document.querySelectorAll(".content");
-const overlay = document.querySelector(".overlay");
-const close = document.querySelector(".close");
 for (var i = 0; i < tabs.length; i++) {
     tabs[i].addEventListener("click", function () {
         for (var j = 0; j < tabs.length; j++) {
             tabs[j].classList.remove("active");
-            content[j].classList.remove("active");
+        }
+        for (var k = 0; k < content.length; k++) {
+            content[k].classList.remove("active");
         }
 
         this.classList.add("active");
@@ -15,23 +15,6 @@ for (var i = 0; i < tabs.length; i++) {
         const idcontent = document.querySelector(`[data-content = "${idtab}"]`);
 
         idcontent.classList.add("active");
-
-        overlay.classList.add("show");
     })
 }
-close.addEventListener("click", function () {
-    overlay.classList.remove("show");
-    for (var i = 0; i < tabs.length; i++) {
-        tabs[i].classList.remove("active");
-        content[i].classList.remove("active");
-    }
-});
-overlay.addEventListener("click", function (e) {
-    if (e.target === overlay) {
-        overlay.classList.remove("show");
-        for (var i = 0; i < tabs.length; i++) {
-            tabs[i].classList.remove("active");
-            content[i].classList.remove("active");
-        }
-    }
-})
+
